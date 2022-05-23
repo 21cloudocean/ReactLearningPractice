ff7d8e57c4e2d79fa01f1e45a34e447ed18fbb59
21cloudocean <102290668+21cloudocean@users.noreply.github.com>
2022-05-23 18:59:27 +0800

    add comments on using props to pass function

--- todolist-passOverFunctionFromChildComponent/src/components/TodoItem.jsx
+++ todolist-passOverFunctionFromChildComponent/src/components/TodoItem.jsx
@@ -6,6 +6,8 @@ function TodoItem(props) {
         props.onChecked(props.id);
       }}
     >
+      //props也可以用来传送function。
+      //这里不能直接写conclick={props.onChecked(props.id)}，会直接调用，而不仅仅是pass over。
       {props.content}
     </li>
   );
